319eb2064b18940de3bb8a49fb2f60ccb438b91f
Yuki-chan <yuki@example.com>
2025-04-06 10:47:55 +0900

    Fix: ルートに index.html を返すようにした💓

--- main.py
+++ main.py
@@ -4,8 +4,19 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-app.mount("/", StaticFiles(directory=".", html=True), name="static")
+# 'static' フォルダにHTMLと画像をまとめる想定
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 @app.get("/")
-def root():
-    return FileResponse("index.html")
+def read_index():
+    return FileResponse("static/index.html")
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return FileResponse(os.path.join(os.path.dirname(__file__), "index.html"))
+
